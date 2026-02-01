6e480aca3d7b526e60267084aad0c15642874e37
Yuji-developer <ykajikawa49378@gmail.com>
2026-02-01 11:47:41 +0900

    Add Slider

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,23 +1,31 @@
 using UnityEngine;
-
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     private AudioSource _audioSource;
 
+    public Slider slider;
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.Play();
+        slider.maxValue = _audioSource.clip.length;
     }
 
 
     // Update is called once per frame
     void Update()
     {
+        //Debug.Log(_audioSource.time);
         if (!_audioSource.isPlaying)
         {
             Debug.Log("ÉQÅ[ÉÄèIóπ");
+        }
+        else
+        {
+            slider.value = _audioSource.time;
         }
     }
 }
