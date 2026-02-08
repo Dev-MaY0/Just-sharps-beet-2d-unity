7d801600478bbaa82f6b587001d1023e3eff45bf
Yuji-developer <ykajikawa49378@gmail.com>
2026-02-08 11:49:09 +0900

    Add Game Cliear

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,25 @@
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class GameManager : MonoBehaviour
 {
     private AudioSource _audioSource;
 
     public Slider slider;
+    public GameObject cliearText;
+
+    private bool _isGameCliear = false;
     private void Awake()
     {
         _audioSource = GetComponent<AudioSource>();
         _audioSource.Play();
         slider.maxValue = _audioSource.clip.length;
+        cliearText.SetActive(false);
+        _isGameCliear = false ;
     }
 
 
@@ -19,13 +27,22 @@ public class GameManager : MonoBehaviour
     void Update()
     {
         //Debug.Log(_audioSource.time);
-        if (!_audioSource.isPlaying)
+        if (!_audioSource.isPlaying && !_isGameCliear)
         {
-            Debug.Log("ÉQÅ[ÉÄèIóπ");
+            _isGameCliear = true;
+            StartCoroutine(GameCliear());
         }
         else
         {
             slider.value = _audioSource.time;
         }
+    }
+
+    private IEnumerator GameCliear()
+    {
+        cliearText.SetActive(true);
+        yield return new WaitForSeconds(2.0f);
+        SceneManager.LoadScene("main");
+        yield break;
     }
 }
